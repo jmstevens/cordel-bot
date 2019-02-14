d52dc12978064c3dc109720cdcd11524e45b5b78
jmstevens <jmstevens1323@gmail.com>
2019-02-14 12:43:31 -0600

    Initial commit

---
+++ cordel-bot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Discord conversational chatbot sandbox project. All code is written following Matthew Inkawhich's pytorch tutorial on developing chatbots https://pytorch.org/tutorials/beginner/chatbot_tutorial.html',
+    author='Joel Stevens',
+    license='MIT',
+)
